17e3064d11551a4774462a469058a3aac9abe8f3
Nimira43 <n.rabone72@aol.com>
2024-08-18 12:27:37 +0100

    v-model

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { ref } from 'vue'
 const name = ref('Lenny')
 const status = ref('sleeping')
 const tasks = ref(['Task 1', 'Task 2', 'Task 3'])
+const newTask = ref('')
 
 const toggleStatus = () => {
   if (status.value === 'active') {
@@ -22,14 +23,27 @@ const toggleStatus = () => {
 </script>
 
 <template>
+  
   <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input 
+      type="text" 
+      id="newTask" 
+      name="newTask"
+      v-model="newTask"
+      >
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
   <button @click="toggleStatus">Change Status</button>
+
 </template>
 
