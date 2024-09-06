63838fd5db1e207a1fdaafccdf6a1a21256f6b5c
Nimira43 <n.rabone72@aol.com>
2024-09-06 21:49:38 +0100

    Started working on single Job Listing component.

--- src/components/BrowseJobs.vue
+++ src/components/BrowseJobs.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import JobListing from './JobListing.vue'
 import jobData from '@/jobs.json'
 import { ref } from 'vue'
 
@@ -15,40 +16,14 @@ console.log(jobs.value)
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div v-for="job in jobs" :key="job.id">
-          {{ job.title }}
-        </div>
+        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+        
       </div>
     </div>
   </section>
     
 <!-- 
-  <div class="bg-[#111] rounded-xl shadow-md relative">
-    <div class="p-4">
-      <div class="mb-6">
-        <div class="text-[#ff4500] my-2">Full-Time</div>
-        <h3 class="text-[#eee] text-xl font-bold">Senior Angular Developer</h3>
-      </div>
-      <div class="text-[#eee] mb-5">
-        We are seeking a talented Front-End Developer to join our team
-        in Leeds. The ideal candidate will have strong skills in multiple JavaScript and CSS frameworks.
-      </div>
-      <h3 class="text-[#ff4500] mb-2">£50-£60K / Year</h3>
-      <div class="border border-[#eee] mb-5"></div>
-      <div class="flex flex-col lg:flex-row justify-between mb-4">
-        <div class="text-[#eee] mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
-          Leeds
-        </div>
-        <a
-          href="job.html"
-          class="h-[36px] bg-[#ff4500] hover:text-[#eee] text-[#111] px-4 py-2 rounded-lg text-center text-sm"
-        >
-          Read More
-        </a>
-      </div>
-    </div>
-  </div>
+  
 
   <div class="bg-[#eee] rounded-xl shadow-md relative">
     <div class="p-4">
