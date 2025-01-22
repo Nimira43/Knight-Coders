82de67c545bde58e9747ba4e686325f20be35df8
Nimira43 <n.rabone72@aol.com>
2025-01-22 07:26:29 +0000

    Toggling truncated text with full text for job description on button click.

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -6,6 +6,9 @@ const props = defineProps({
 })
 
 const showFullDescription = ref(false)
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value
+}
 
 const truncatedDescription = computed(() => {
   let description = props.job.description
@@ -24,9 +27,17 @@ const truncatedDescription = computed(() => {
         <div class='text-[#1c9fd3] my-2'>{{ job.type }}</div>
         <h3 class='text-[#eee] text-xl font-bold'>{{ job.title }}</h3>
       </div>
+
       <div class='text-[#eee] mb-5'>
-        {{ truncatedDescription }}
+        <div>{{ truncatedDescription }}</div>
       </div>
+      <button 
+        @click="toggleFullDescription"
+        class="text-[#1c9fd3] hover:text-[#eee] mb-5"
+      >
+        {{ showFullDescription ? 'Less' : 'More' }}
+      </button>
+      
       <h3 class='text-[#1c9fd3] mb-2'>{{ job.salary }} / Year</h3>
       <div class='border border-[#eee] mb-5'></div>
       <div class='flex flex-col lg:flex-row justify-between mb-4'>
