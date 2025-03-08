3fbdb62e741c47d51217ff6c65916b4c1c164ecf
Nimira43 <n.rabone72@aol.com>
2025-03-08 12:45:57 +0000

    Using RouterLink inside Navbar instead of the a tags

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,44 +1,43 @@
 <script setup>
-
 import logo from '@/assets/images/logo.png'
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <nav class='bg-main border-b-2 border-dark'>
-      <div class='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-        <div class='flex h-20 items-center justify-between'>
-          <div
-            class='flex flex-1 items-center justify-center md:items-stretch md:justify-start'
-          >
-            <a class='flex flex-shrink-0 items-center mr-4' href='index.html'>
-              <img class='h-10 w-auto' :src='logo' alt='Jobs 4 Coders' />
-              <span class='hidden md:block text-light text-3xl ml-2 logo'
-                >Knight Coders</span
+    <div class='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+      <div class='flex h-20 items-center justify-between'>
+        <div class='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+          <RouterLink class='flex flex-shrink-0 items-center mr-4' to='/'>
+            <img class='h-10 w-auto' :src='logo' alt='Jobs 4 Coders' />
+            <span class='hidden md:block text-light text-3xl ml-2 logo'>
+              Knight Coders
+            </span>
+          </RouterLink>
+          <div class='md:ml-auto'>
+            <div class='flex space-x-2'>
+              <RouterLink
+                to='/'
+                class='text-light hover:text-dark rounded-md px-3 py-2'
               >
-            </a>
-            <div class='md:ml-auto'>
-              <div class='flex space-x-2'>
-                <a
-                  href='/'
-                  class='text-light hover:text-dark rounded-md px-3 py-2'
-                  >Home</a
-                >
-                <a
-                  href='/jobs'
-                  class='text-light hover:text-dark rounded-md px-3 py-2'
-                  >Jobs</a
-                >
-                <a
-                  href='/jobs/add'
-                  class='text-light hover:text-dark rounded-md px-3 py-2'
-                  >Add Job</a
-                >
-              </div>
+                Home
+              </RouterLink>
+              <RouterLink
+                to='/jobs'
+                class='text-light hover:text-dark rounded-md px-3 py-2'
+              >
+                Jobs
+              </RouterLink>
+              <RouterLink
+                to='/jobs/add'
+                class='text-light hover:text-dark rounded-md px-3 py-2'
+              >
+                Add Job
+              </RouterLink>
             </div>
           </div>
         </div>
       </div>
-    </nav>
-
+    </div>
+  </nav>
 </template>
