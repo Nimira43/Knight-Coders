a62e792f190f688fe06b278e758a4d2fccd4c4b8
Nimira43 <n.rabone72@aol.com>
2025-03-08 12:18:55 +0000

    Using the bg property again on line 11 of HomeCards.vue.

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -8,7 +8,7 @@ import Card from './Card.vue'
   <section class='bg-dark py-4'>
     <div class='container-xl lg:container m-auto'>
       <div class='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
-        <Card class='bg-main'>
+        <Card bg='bg-main'>
         <h2 class='text-light text-2xl font-bold'>For Developers</h2>
           <p class='text-dark mt-2 mb-4'>
             Django, MERN, Svelte Kit job opportunities plus many more...
