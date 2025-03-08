5e5396755e86ec35b847293f23c2a06800081121
Nimira43 <n.rabone72@aol.com>
2025-03-08 12:35:28 +0000

    Creating the jobs route in router/index.js and updated styling in BrowseJobs.vue

--- src/components/BrowseJobs.vue
+++ src/components/BrowseJobs.vue
@@ -18,9 +18,9 @@ console.log(jobs.value)
 </script>
 
 <template>
-  <section class='bg-main px-4 py-10'>
+  <section class='bg-light px-4 py-10'>
     <div class='container-xl lg:container m-auto'>
-      <h2 class='text-3xl font-bold text-light mb-6 text-center'>
+      <h2 class='text-3xl font-bold text-main mb-6 text-center'>
         Browse Jobs
       </h2>
       <div class='grid grid-cols-1 md:grid-cols-3 gap-6'>
