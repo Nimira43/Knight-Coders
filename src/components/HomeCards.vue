c03bafa4281825fd059d2149aded0cd53b25b22e
Nimira43 <n.rabone72@aol.com>
2025-03-22 21:08:23 +0000

    Update a tags to RouterLinks inside HomeCards.vue

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import { RouterLink } from 'vue-router'
 import Card from './Card.vue'
 
 </script>
@@ -13,24 +13,24 @@ import Card from './Card.vue'
           <p class='text-dark mt-2 mb-4'>
             Django, MERN, Svelte Kit job opportunities plus many more...
           </p>
-          <a
-            href='jobs.html'
+          <RouterLink
+            to='/jobs'
             class='inline-block bg-light text-dark rounded-lg px-4 py-2 hover:text-main'
           >
             Browse Jobs
-          </a>
+          </RouterLink>
         </Card>
         <Card bg='bg-light'>
           <h2 class='text-main text-2xl font-bold'>For Employers</h2>
           <p class='text-dark mt-2 mb-4'>
             List your job to find the perfect developer for the role
           </p>
-          <a
-            href='add-job.html'
+          <RouterLink
+            to='/jobs/add'
             class='inline-block bg-main text-dark rounded-lg px-4 py-2 hover:text-light'
           >
             Add Job
-          </a>
+          </RouterLink>
         </Card>
       </div>
     </div>
