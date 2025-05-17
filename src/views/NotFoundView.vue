d7287ce3c0bac727c13f9fc4103a86fda7b86ca7
Nimira43 <n.rabone72@aol.com>
2025-05-17 19:58:35 +0100

    Set up page not found routing.

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -7,7 +7,7 @@ import { RouterLink } from 'vue-router'
   <section class="text-center flex flex-col justify-center items-center h-96">
     <i class="pi pi-exclamation-triangle text-main text-5xl mb-5"></i>
     <h1 class="text-6xl font-bold mb-4">404</h1>
-    <p class="text-xl mb-5 uppercase">Page Not Found</p>
+    <p class="text-main text-xl mb-5 uppercase">Page Not Found</p>
     <RouterLink
       to="/"
       class="text-light bg-dark hover:bg-grey-dark rounded-md px-3 py-2 mt-4 uppercase"
