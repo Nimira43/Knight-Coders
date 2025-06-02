db064ebbd73e4c747d6ca1dfd4205a7fe4e56a65
Nimira43 <n.rabone72@aol.com>
2025-06-02 08:50:32 +0100

    Small update.

--- src/components/BrowseJobs.vue
+++ src/components/BrowseJobs.vue
@@ -39,7 +39,7 @@ onMounted(async () => {
       <div v-if='state.isLoading' class="text-center text-main py-6">
         <PulserLoader />
       </div>
-      <div class='grid grid-cols-1 md:grid-cols-3 gap-6'>
+      <div v-else class='grid grid-cols-1 md:grid-cols-3 gap-6'>
         <JobListing 
           v-for='job in state.jobs.slice(0, limit || state.jobs.length)' 
           :key='job.id' 
