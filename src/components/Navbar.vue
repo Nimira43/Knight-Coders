46c035c408555f6dda2617ca35229dd547356309
Nimira43 <n.rabone72@aol.com>
2025-06-18 17:50:37 +0100

    Removed the bottom border on the Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@ const isActiveLink = (routePath) => {
 </script>
 
 <template>
-  <nav class='bg-main border-b-2 border-dark'>
+  <nav class='bg-main'>
     <div class='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
       <div class='flex h-20 items-center justify-between'>
         <div class='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
