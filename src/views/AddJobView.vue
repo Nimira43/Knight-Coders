61e12c874db79b00bbf1d3c50978e6f3c6de3ef1
Nimira43 <n.rabone72@aol.com>
2025-07-03 08:25:19 +0100

    Setting up container.

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,3 +1,9 @@
 <template>
-  Add Job
+  <section className='bg-light'>
+    <div class="container m-auto max-w-2xl py-24">
+      <div class="bg-grey-light px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+        
+      </div>
+    </div>
+  </section>
 </template>
