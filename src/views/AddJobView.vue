76d31c90e808af8daa3567bc9e7293a537e1d200
Nimira43 <n.rabone72@aol.com>
2025-07-03 08:45:32 +0100

    Setting up the Job Type section of the page.

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,10 +1,24 @@
 <template>
-  <section className='bg-light'>
+  <section class='bg-light'>
     <div class="container m-auto max-w-2xl py-24">
       <div class="bg-grey-light px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
         <form>
           <h2 class="text-3xl text-center font-medium mb-6">Add Job</h2>
-          
+          <div class="mb-4">
+            <label for="type" class="block text-grey-dark font-medium mb-2">Job Type</label>
+            <select 
+              required
+              name="type" 
+              id="type" 
+              class="border rounded-md w-full py-2 px-3"
+            >
+            <option value="Full-Time">Full Time</option>
+            <option value="Part-Time">Part Time</option>
+            <option value="Remote">Remote</option>
+            <option value="Placement">Placement</option>
+            <option value="Temporary">Temporary</option>
+            </select>
+          </div>
         </form>
       </div>
     </div>
