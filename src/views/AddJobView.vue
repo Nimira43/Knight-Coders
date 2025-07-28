c22f1074b13552d46f30cd1d421cff11a4595589
Nimira43 <n.rabone72@aol.com>
2025-07-28 04:37:36 +0100

    Calling handleSubmit for form

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -20,7 +20,7 @@ const form = reactive({
   <section class='bg-light'>
     <div class='container m-auto max-w-2xl py-24'>
       <div class='bg-grey-light px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-        <form>
+        <form @submit.prevent='handleSubmit'>
           <h2 class='text-3xl text-center font-semibold mb-6 text-dark uppercase'>Add Job</h2>
           <div class='mb-4'>
             <label 
